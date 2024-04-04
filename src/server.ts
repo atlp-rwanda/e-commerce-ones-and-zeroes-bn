d2ac1a8b5991b2246b70ee5ff65a5ef4aa814784
pacifiquemboni <pacifiquemboni@gmail.com>
2024-04-04 14:58:51 +0200

    bg(Fix: Hound suggestion of using eslint)

--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,8 @@ const app = express();
 app.get('/', (req, res) => {
   res.send('Hello, Express with TypeScript!');
 });
-
+/* eslint-disable-next-line */
+/* eslint-env es6 */
 const PORT = process.env.PORT || 7000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
