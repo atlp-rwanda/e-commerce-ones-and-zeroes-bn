5477eb33abb5d423e2ea9d7a0147101f65c575dc
Celestin4 <nteziryayocelestin4@gmail.com>
2024-04-06 16:46:36 +0200

    [fixes #187355082] Set up an empty Express Boilerplate with dotenv

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,16 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import routes from './routes/index'
+
 
 dotenv.config();
 
 const app = express();
+const port = process.env.PORT || 7000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express with TypeScript!');
-});
-/* eslint-disable-next-line */
-/* eslint-env es6 */
-const PORT = process.env.PORT || 7000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.use('/', routes);
+
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
