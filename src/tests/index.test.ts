670502be2764a63f6b3089255d91d75f112221fa
Igor IHIMBAZWE <igorihimbazwe@Igors-MacBook-Pro.local>
2024-04-30 20:47:01 +0200

    Finishes [#187355089] circle ci badge

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -1,19 +1,15 @@
 import express, { Router, Request, Response } from 'express';
 import request from 'supertest'; // Import the request function from supertest
+import exampleRoutes from '../routes/exampleRoutes'; // Import your exampleRoutes
 
 const app = express();
-const router = Router(); // Create a new Router instance
 
-// Define your routes
-router.get('/', (req: Request, res: Response) => {
-  res.status(200).json({ message: 'success' });
-});
-
-app.use('/', router);
+// Use your imported routes
+app.use('/', exampleRoutes);
 
 test('GET / should return status 200 and a JSON response', async () => {
   const response = await request(app).get('/');
   
   expect(response.status).toBe(200);
-  expect(response.body).toEqual({ message: 'success' });
+  expect(response.body).toEqual({ message: 'sucess' });
 });
