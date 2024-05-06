a66929020cf23d5719cb4100276dd4190a35825e
Shema Serge <shemaallansurge@gmail.com>
2024-05-06 09:28:54 +0200

    remove duplicates in package.json

--- src/tests/index.test.ts
+++ src/tests/index.test.ts
@@ -9,10 +9,10 @@ app.use('/', exampleRoutes);
 
 test('GET / should return status 200 and a JSON response', async () => {
   const response = await request(app).get('/');
-
   expect(response.status).toBe(200);
   expect(response.body).toEqual({ message: 'sucess' });
 }),
-  test('1 + 2 should be 3', () => {
-    expect(add2Numbers(1, 2)).toBe(3);
-  });
+
+test('1 + 2 should be 3', () => {
+  expect(add2Numbers(1, 2)).toBe(3);
+});
