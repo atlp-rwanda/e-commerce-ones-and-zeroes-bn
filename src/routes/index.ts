5db7d129b524bac2664f668a078434e206effc54
pacifiquemboni <pacifiquemboni@gmail.com>
2024-05-22 13:20:16 +0200

    [Deliver #187355114] ft-seller-updating-product
    ssss
    
    [Finishes #187355094]A successfully registered user should receive an email to verify their account email
    
    Finishes [#187355103] Resetting password
    
    Update users.ts
    
    [Finishes #187355101] User Registration and Login By Google
    
    [Fixes #187355107] ft-disable-account
    
    changes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -10,6 +10,8 @@ router.get('/', (req: Request, res: Response) => {
 // Use userRoute for paths starting with '/users'
 router.use('/users', userRoute);
 router.use('/products', productRouter);
+router.use('/user', userRoute);
+
 // Error handling middleware
 router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   console.error(err.stack);
